d368fd02643fee018f95e93bef36f20c86d6c57b
Assil94 <assil.heddar1@gmail.com>
2021-10-02 02:17:20 +0200

    avancement

---
+++ SimpleTravel/Controller/PresentTranslateController.swift
@@ -0,0 +1,8 @@
+//
+//  PresentTranslateController.swift
+//  SimpleTravel
+//
+//  Created by Assil Heddar on 30/09/2021.
+//
+
+import Foundation
---
+++ SimpleTravel/Extension/UITextView.swift
@@ -0,0 +1,8 @@
+//
+//  UITextView.swift
+//  SimpleTravel
+//
+//  Created by Assil Heddar on 01/10/2021.
+//
+
+import Foundation
